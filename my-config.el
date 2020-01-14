56c720db04e09c1a02d6e105b1cb79969a7cfcbc
Christopher Dempewolf <chris@chrisdempewolf.com>
2020-01-14 08:23:41 -0600

    Change tabs to spaces; move evil packages inside evil's use-package

--- my-config.el
+++ my-config.el
@@ -34,186 +34,186 @@
 
 ;; ~~~~~~~~~~~~~~~~~~~~~~~ PACKAGE CONFIGURATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-												 ("melpa" . "https://melpa.org/packages/")
-												 ("org" . "http://orgmode.org/elpa/")))
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 (require 'package)
 (package-initialize)
 (package-refresh-contents 1)                  ; Refresh packages asynchronously
 
 ;; Setup use-package
 (eval-when-compile
-	(require 'use-package))
+  (require 'use-package))
 (setq use-package-compute-statistics 1)       ; Generate a report of load times with M-x use-package-report
 
 (use-package anzu
-	:diminish
-	:config
-	(global-anzu-mode 1))
+  :diminish
+  :config
+  (global-anzu-mode 1))
 
 (use-package darkroom
   :ensure t
-	:demand t
+  :demand t
   :config
   (setq darkroom-text-scale-increase 1))
 
 (use-package desktop+
-	:diminish
-	:config
+  :diminish
+  :config
   :demand t
   :config
   (desktop-save-mode 1))
 
 (use-package diminish
-	:config
-	(diminish 'undo-tree-mode)
-	(diminish 'eldoc-mode))
+  :config
+  (diminish 'undo-tree-mode)
+  (diminish 'eldoc-mode))
 
 (use-package evil
-	:ensure evil
-	:config
-	(evil-mode 1)
+  :ensure evil
+  :config
+  (evil-mode 1)
 
-	(dolist (mode
-					  '(git-rebase-mode
+  (dolist (mode
+            '(git-rebase-mode
               flycheck-error-list-mode
-				      osx-dictionary-mode
+              osx-dictionary-mode
               term-mode))
   (add-to-list 'evil-emacs-state-modes mode))
 
-	(setq evil-search-wrap t
-				evil-regexp-search t)
-	(setq evil-want-C-u-scroll t) 
-	(setq-default tab-width 2)
+  (setq evil-search-wrap t
+        evil-regexp-search t)
+  (setq evil-want-C-u-scroll t)
+  (setq-default tab-width 2)
 
-	(defvar my-leader-map (make-sparse-keymap)
-		"Keymap for \"leader key\" shortcuts.")
+  (defvar my-leader-map (make-sparse-keymap)
+    "Keymap for \"leader key\" shortcuts.")
 
-	;; binding "," to the keymap
-	(define-key evil-normal-state-map "," my-leader-map)
+  ;; binding "," to the keymap
+  (define-key evil-normal-state-map "," my-leader-map)
 
-	;; change the "leader" key to space
-	(define-key evil-normal-state-map "," 'evil-repeat-find-char-reverse)
-	(define-key evil-normal-state-map (kbd "SPC") my-leader-map))
+  ;; change the "leader" key to space
+  (define-key evil-normal-state-map "," 'evil-repeat-find-char-reverse)
+  (define-key evil-normal-state-map (kbd "SPC") my-leader-map)
+
+  (use-package evil-commentary
+    :diminish
+    :config
+    (evil-commentary-mode))
+
+  (use-package evil-magit
+    :after evil
+    :after magit
+    :diminish
+    :config
+    (evil-define-key evil-magit-state magit-mode-map "?" 'evil-search-backward))
+
+  (use-package evil-mc
+    :diminish
+    :config
+    (global-evil-mc-mode 1))
+
+  (use-package evil-rsi
+    :diminish
+    :config
+    (evil-rsi-mode))
+
+  (use-package evil-surround
+    :ensure t
+    :config
+    (global-evil-surround-mode 1)))
 
 (use-package ace-jump-mode
-	:after evil                                 ; Should make this order-agnostic, but this does not appear to work
-	:init
-	(define-key my-leader-map "aj" 'ace-jump-mode))
-
-(use-package evil-commentary
-	:diminish
-	:config
-	(evil-commentary-mode))
-
-(use-package evil-magit
-	:after evil
-	:after magit
-	:diminish
-	:config
-	(evil-define-key evil-magit-state magit-mode-map "?" 'evil-search-backward))
-
-(use-package evil-mc
-	:diminish
-	:config
-	(global-evil-mc-mode 1))
-
-(use-package evil-rsi
-	:diminish
-	:config
-	(evil-rsi-mode))
-
-(use-package evil-surround
-	:ensure t
-	:config
-	(global-evil-surround-mode 1))
+  :after evil                                 ; Should make this order-agnostic, but this does not appear to work
+  :init
+  (define-key my-leader-map "aj" 'ace-jump-mode)  
 
 (use-package flycheck
-	:diminish
-	:config
-	(global-flycheck-mode)
-	(define-key my-leader-map "en" 'next-error)
-	(define-key my-leader-map "ep" 'previous-error))
+  :diminish
+  :config
+  (global-flycheck-mode)
+  (define-key my-leader-map "en" 'next-error)
+  (define-key my-leader-map "ep" 'previous-error))
 
 ;; Bind <SPC c> to the M-x function (with Helm).
 ;; I'm using c, because I cannot get <SPC SPC> (what Spacemacs uses) to work.
 ;;(define-key my-leader-map "c" 'execute-extended-command)
 (use-package helm
-	:config
-	(setq
-	 helm-always-two-windows t
-	 helm-split-window-default-side 'left)
-	(define-key my-leader-map "c" 'helm-M-x))
+  :config
+  (setq
+   helm-always-two-windows t
+   helm-split-window-default-side 'left)
+  (define-key my-leader-map "c" 'helm-M-x))
 
 (use-package helm-flx
-	:config
-	(helm-flx-mode 1)
-	(setq helm-flx-for-helm-find-files t
-				helm-flx-for-helm-locate t))
+  :config
+  (helm-flx-mode 1)
+  (setq helm-flx-for-helm-find-files t
+        helm-flx-for-helm-locate t))
 
 ;; needs more setup. does not work.
 (use-package helm-fuzzier
-	:config
-	(helm-fuzzier-mode 1))
+  :config
+  (helm-fuzzier-mode 1))
 
 (use-package ido-completing-read+
-	:init
-	(ido-mode 1)
+  :init
+  (ido-mode 1)
   (ido-everywhere 1)
-	:config
-	(ido-ubiquitous-mode 1))
+  :config
+  (ido-ubiquitous-mode 1))
 
 (use-package paradox
-	:config
-	(paradox-enable))
+  :config
+  (paradox-enable))
 
 (use-package rainbow-delimiters
-	:hook (prog-mode . rainbow-delimiters-mode))
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package solarized-theme
-	:init
-	;; make the fringe stand out from the background
-	(setq solarized-distinct-fringe-background t)
+  :init
+  ;; make the fringe stand out from the background
+  (setq solarized-distinct-fringe-background t)
 
-	;; Don't change the font for some headings and titles
-	(setq solarized-use-variable-pitch nil)
+  ;; Don't change the font for some headings and titles
+  (setq solarized-use-variable-pitch nil)
 
-	;; make the modeline high contrast
-	(setq solarized-high-contrast-mode-line nil)
+  ;; make the modeline high contrast
+  (setq solarized-high-contrast-mode-line nil)
 
-	;; Use less bolding
-	(setq solarized-use-less-bold t)
+  ;; Use less bolding
+  (setq solarized-use-less-bold t)
 
-	;; Use more italics
-	(setq solarized-use-more-italic t)
+  ;; Use more italics
+  (setq solarized-use-more-italic t)
 
-	;; Avoid all font-size changes
-	(setq solarized-height-minus-1 1.0)
-	(setq solarized-height-plus-1 1.0)
-	(setq solarized-height-plus-2 1.0)
-	(setq solarized-height-plus-3 1.0)
-	(setq solarized-height-plus-4 1.0)
+  ;; Avoid all font-size changes
+  (setq solarized-height-minus-1 1.0)
+  (setq solarized-height-plus-1 1.0)
+  (setq solarized-height-plus-2 1.0)
+  (setq solarized-height-plus-3 1.0)
+  (setq solarized-height-plus-4 1.0)
 
-	:config
-	(define-key my-leader-map "ltsl" (lambda () (interactive) (load-theme 'solarized-light t)))
-	(define-key my-leader-map "ltsd" (lambda () (interactive) (load-theme 'solarized-dark t))))
+  :config
+  (define-key my-leader-map "ltsl" (lambda () (interactive) (load-theme 'solarized-light t)))
+  (define-key my-leader-map "ltsd" (lambda () (interactive) (load-theme 'solarized-dark t))))
 
 (use-package spacemacs-theme
   :defer t
-	:init
-	(setq spacemacs-theme-comment-bg nil)
-	(setq spacemacs-theme-org-bold nil)
-	(setq spacemacs-theme-org-height nil)
-	(setq spacemacs-theme-org-highlight nil)
-	(setq spacemacs-theme-org-priority-bold nil)
+  :init
+  (setq spacemacs-theme-comment-bg nil)
+  (setq spacemacs-theme-org-bold nil)
+  (setq spacemacs-theme-org-height nil)
+  (setq spacemacs-theme-org-highlight nil)
+  (setq spacemacs-theme-org-priority-bold nil)
 
-	(define-key my-leader-map "ltl" (lambda () (interactive) (load-theme 'spacemacs-light t)))
-	(define-key my-leader-map "ltd" 'cld/load-spacemacs-dark-theme))
+  (define-key my-leader-map "ltl" (lambda () (interactive) (load-theme 'spacemacs-light t)))
+  (define-key my-leader-map "ltd" 'cld/load-spacemacs-dark-theme))
 
 (use-package which-key
-	:diminish
-	:config
-	(which-key-setup-side-window-right-bottom)
-	(which-key-mode))
+  :diminish
+  :config
+  (which-key-setup-side-window-right-bottom)
+  (which-key-mode))
 
 (cld/load-spacemacs-dark-theme)
 
