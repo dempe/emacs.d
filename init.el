7a30323b9807eeaccadfe401a8658aa88a994bc1
Christopher Dempewolf <chris@chrisdempewolf.com>
2020-02-20 21:15:03 -0600

    readd bkp files

--- init.el
+++ init.el
@@ -13,7 +13,6 @@
 (setq recentf-max-saved-items 25)
 
 (setq ispell-program-name "/usr/local/bin/ispell")
-(setq make-backup-files nil)                   ; I use git for pretty much everything
 (setq initial-scratch-message "")
 (setq truncate-lines nil)                      ; Turn on word wrap
 (setq org-startup-truncated nil)               ; org-mode does not obey general word wrap setting
@@ -173,7 +172,7 @@
   (which-key-setup-side-window-right-bottom)
   (which-key-mode))
 
-(cld/load-spacemacs-dark-theme)
+;; (cld/load-spacemacs-dark-theme)
 
 ;; ~~~~~~~~~~~~~~~~~~~~~~~ KEYBINDINGS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -338,3 +337,35 @@
 
 (provide 'init.el)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(electric-pair-mode t)
+ '(package-selected-packages
+   (quote
+    (org-trello poker evil-magit spacemacs-theme spotlight osx-dictionary markdown-mode ido-completing-read+ desktop+ speed-type darkroom evil-surround evil-rsi evil-commentary solarized-theme helm-fuzzier helm-flx diminish use-package evil-mc paradox rainbow-delimiters anzu flycheck swiper magit ace-jump-mode helm which-key evil)))
+ '(safe-local-variable-values
+   (quote
+    ((org-todo-keyword-faces
+      ("GOOD" . "green")
+      ("OKAY" . "yellow")
+      ("BAD" . "red"))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face :foreground "#ff0000"))))
+ '(rainbow-delimiters-depth-2-face ((t (:inherit rainbow-delimiters-base-face :foreground "#ffa500"))))
+ '(rainbow-delimiters-depth-3-face ((t (:inherit rainbow-delimiters-base-face :foreground "#ffff00"))))
+ '(rainbow-delimiters-depth-4-face ((t (:inherit rainbow-delimiters-base-face :foreground "#00ff00"))))
+ '(rainbow-delimiters-depth-5-face ((t (:inherit rainbow-delimiters-base-face :foreground "#0000ff"))))
+ '(rainbow-delimiters-depth-6-face ((t (:inherit rainbow-delimiters-base-face :foreground "#ff00ff"))))
+ '(rainbow-delimiters-depth-7-face ((t (:inherit rainbow-delimiters-base-face :foreground "#ffffff"))))
+ '(rainbow-delimiters-depth-8-face ((t (:inherit rainbow-delimiters-base-face :foreground "#00ff00"))))
+ '(rainbow-delimiters-depth-9-face ((t (:inherit rainbow-delimiters-base-face :foreground "#89cffa")))))
